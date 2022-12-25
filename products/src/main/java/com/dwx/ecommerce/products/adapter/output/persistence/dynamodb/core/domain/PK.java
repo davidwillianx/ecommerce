5ccdf18da69fc8f2fe3fb1e66b3a0420715f6e0e
davidwillianx <davidcbsi@gmail.com>
2022-12-25 09:45:03 -0300

    feat: identity mapping

--- products/src/main/java/com/dwx/ecommerce/products/adapter/output/persistence/dynamodb/core/domain/PK.java
+++ products/src/main/java/com/dwx/ecommerce/products/adapter/output/persistence/dynamodb/core/domain/PK.java
@@ -1,2 +1,20 @@
-package com.dwx.ecommerce.products.adapter.output.persistence.dynamodb.core.domain;public class PK {
+package com.dwx.ecommerce.products.adapter.output.persistence.dynamodb.core.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+@Builder
+@Getter
+public class PK {
+    private String PK;
+    private String SK;
+
+    //TODO: Move to an interface or abstract class
+    public Map<String, Object> getId() {
+        return Map.of(
+                "PK", this.PK,
+                 "SK", this.SK
+        );
+    }
 }
