59c4fd0c7dab44d74f6aa42d123108ce61767aa5
davidwillianx <davidcbsi@gmail.com>
2022-12-25 09:31:50 -0300

    feat: bean registry

--- products/src/main/java/com/dwx/ecommerce/products/config/aws/AwsInitializer.java
+++ products/src/main/java/com/dwx/ecommerce/products/config/aws/AwsInitializer.java
@@ -1,20 +1,26 @@
 package com.dwx.ecommerce.products.config.aws;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 
 @Configuration
 @RequiredArgsConstructor
 public class AwsInitializer {
-    private final AwsProperties awsProperties;
-
-    public AmazonDynamoDB amazonDynamoDB() {
-      return  AmazonDynamoDBClientBuilder
+    @Primary
+    @Bean
+    public AmazonDynamoDBAsync amazonDynamoDB(final AwsProperties awsProperties) {
+        final var basicCredentials = new BasicAWSCredentials("LOCALSTACK_KEY", "LOCALSTACK_SECRET");
+        return AmazonDynamoDBAsyncClientBuilder
                 .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(basicCredentials))
                 .withEndpointConfiguration(
                         new AwsClientBuilder.EndpointConfiguration(
                                 awsProperties.dynamo().endpoint(),
