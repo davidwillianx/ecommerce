9e9c12736d5b01c9227fe59f33daa5c09011c426
davidwillianx <davidcbsi@gmail.com>
2022-12-25 09:44:40 -0300

    feat: add base query result

--- products/src/main/java/com/dwx/ecommerce/products/adapter/output/persistence/dynamodb/core/domain/Model.java
+++ products/src/main/java/com/dwx/ecommerce/products/adapter/output/persistence/dynamodb/core/domain/Model.java
@@ -1,2 +1,8 @@
-package com.dwx.ecommerce.products.adapter.output.persistence.dynamodb.core.domain;public interface Model {
+package com.dwx.ecommerce.products.adapter.output.persistence.dynamodb.core.domain;
+
+import java.util.Map;
+
+public interface Model {
+    PK getId();
+    Map<String, Object> getItem();
 }
