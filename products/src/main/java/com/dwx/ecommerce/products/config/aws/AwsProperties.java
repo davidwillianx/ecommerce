0723794636f96cd8b9831032ef73fe8827f937a3
davidwillianx <davidcbsi@gmail.com>
2022-12-25 09:34:39 -0300

    feat: aws configurations handler

--- products/src/main/java/com/dwx/ecommerce/products/config/aws/AwsProperties.java
+++ products/src/main/java/com/dwx/ecommerce/products/config/aws/AwsProperties.java
@@ -1,10 +1,12 @@
 package com.dwx.ecommerce.products.config.aws;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
+@ConstructorBinding
 @ConfigurationProperties(prefix = "aws")
 public record AwsProperties (
+        String endpoint,
         String region,
         DynamoDBProperties dynamo
 ) {}
