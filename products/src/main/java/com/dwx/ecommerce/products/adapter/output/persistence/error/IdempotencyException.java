1fe9127b640cde62fd20ab87dd1b5df39e20e811
davidwillianx <davidcbsi@gmail.com>
2023-01-30 22:24:07 -0300

    feat: add catchable errors

--- products/src/main/java/com/dwx/ecommerce/products/adapter/output/persistence/error/IdempotencyException.java
+++ products/src/main/java/com/dwx/ecommerce/products/adapter/output/persistence/error/IdempotencyException.java
@@ -1,10 +1,18 @@
 package com.dwx.ecommerce.products.adapter.output.persistence.error;
 
+import com.dwx.ecommerce.products.adapter.output.persistence.core.error.Error;
 import lombok.Getter;
 
 @Getter
-public class IdempotencyException extends PersistenceException{
-    public IdempotencyException(String code, String message) {
-        super(code, message, null);
+public class IdempotencyException extends PersistenceException {
+    private static final Error error = Error.NON_UNIQUE_TRANSACTION;
+    private static final String DEFAULT_MESSAGE = "Idempotency check already in use";
+
+    public IdempotencyException() {
+        super(
+                error.getCode(),
+                DEFAULT_MESSAGE,
+                null
+        );
     }
 }
